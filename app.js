efe4e02e1d686850f37637418158b4acc157f28d
Dimitar Kokotanekov <acid901@mail.bg>
2018-04-29 18:54:28 +0300

    add roll the dice button functionality

--- app.js
+++ app.js
@@ -9,16 +9,38 @@ GAME RULES:
 
 */
 
-let scores, roundSroce, activePlayer, dice;
+let scores, roundSroce, activePlayer;
 
 scores = [0,0];
 roundSroce = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+	
+	//random number
+	var dice = Math.floor(Math.random() * 6 ) + 1;
+
+
+	//display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png'; 
+
+
+
+
+
+} );
